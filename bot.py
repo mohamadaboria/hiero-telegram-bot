2b51e122c3c2fb4fb7b5ea7276080ea42c0d8dea
מוחמד אבוריא <mh2404@h-MacBook-Air-sl-mwhmd-bwry.local>
2025-04-20 16:55:19 +0300

    update bot rtl

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ from telegram import Update, InputFile
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 from PIL import Image, ImageDraw, ImageFont
 
-
 # تحويل الحروف العربية إلى رموز هيروغليفية
 arabic_to_hieroglyphs = {
     'ا': '𓄿', 'ب': '𓃀', 'ت': '𓏏', 'ث': '𓍿',
@@ -23,26 +22,33 @@ def translate_to_hieroglyphs(text):
     return ''.join(arabic_to_hieroglyphs.get(char, char) for char in text)
 
 def create_hieroglyph_image(text, user_id):
+    reversed_text = text[::-1]  # نعكس النص ليرسم من اليمين
     img = Image.new('RGB', (600, 200), color=(255, 255, 255))
     draw = ImageDraw.Draw(img)
     try:
         font = ImageFont.truetype("NotoSansEgyptianHieroglyphs-Regular.ttf", 50)
     except:
         font = ImageFont.load_default()
-    draw.text((20, 70), text, font=font, fill=(0, 0, 0))
+    try:
+        bbox = draw.textbbox((0, 0), reversed_text, font=font)
+        text_width = bbox[2] - bbox[0]
+    except:
+        text_width = 0
+    x_position = 600 - text_width - 20
+    draw.text((x_position, 70), reversed_text, font=font, fill=(0, 0, 0))
     filepath = f"{user_id}_hiero.png"
     img.save(filepath)
     return filepath
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("مرحبًا بك في بوت الهيروغليفية الفرعوني!أرسل لي اسمك باللغة العربية، وسأقوم بتحويله إلى رموز هيروغليفية قديمة وسأرسل لك صورة تحتوي على الاسم مرسومًا.")
+    await update.message.reply_text("مرحبًا بك في بوت الهيروغليفية الفرعوني! أرسل لي اسمك باللغة العربية، وسأقوم بتحويله إلى رموز هيروغليفية قديمة وسأرسل لك صورة تحتوي على الاسم مرسومًا.")
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     uid = update.effective_user.id
     arabic_text = update.message.text
     hiero_text = translate_to_hieroglyphs(arabic_text)
 
-    await update.message.reply_text("اسمك بالهيروغليفية:\n" + hiero_text)
+    await update.message.reply_text("اسمك بالهيروغليفية:" + hiero_text[::-1])
 
     image_path = create_hieroglyph_image(hiero_text, uid)
     with open(image_path, 'rb') as img:
